87c66e512568f386af560f09425f93cc50012c5d
Yuliya <iksulia1@gmail.com>
2023-03-30 18:25:58 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        init();
+    }
+    public static void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try{
+            telegramBotsApi.registerBot(new Bot());
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+}
+}
---
+++ src/main/java/org/example/Storage.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Storage {
+
+    private ArrayList<String> quoteList;
+    Storage()
+    {
+        parser();
+    }
+
+    String getRandQuote()
+    {
+        //получаем случайное значение в интервале от 0 до самого большого индекса
+        int randValue = (int)(Math.random() * quoteList.size());
+
+        //Из коллекции получаем цитату со случайным индексом и возвращаем ее
+        return quoteList.get(randValue);
+    }
+    void parser()
+    {
+        quoteList = new ArrayList<>();
+        String classNmae = "su-note-inner su-u-clearfix su-u-trim";
+        Document doc = null;
+        try {
+            //Получаем документ нужной нам страницы
+            doc = Jsoup.connect("https://citatnica.ru/citaty/mudrye-tsitaty-velikih-lyudej").maxBodySize(0).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Получаем группу объектов, обращаясь методом из Jsoup к определенному блоку
+        Elements elQuote = doc.getElementsByClass(classNmae);
+
+        //Достаем текст из каждого объекта поочереди и добавляем в наше хранилище
+        elQuote.forEach(el -> {
+            quoteList.add(el.text());
+        });
+    }
+}
